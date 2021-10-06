ebaf75964f00ef56de238d203822e21e8dc855f3
damian <damiankohut@gmail.com>
2021-10-06 14:58:10 +0000

    answer to question 1

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(num){
+     if(typeof num === "number"){
+         if(num < 0){
+            return(num * -1)
+         }else{
+            return num  
+        }
+    } else{
+        return null
+    }
 }
 
+//console.log(absoluteValue(-.5))
 
-
-
+function multipleOfFourAndSix()
+    if (let i >= 1; i < 100; ++1){
+    
+     
+    
+}
